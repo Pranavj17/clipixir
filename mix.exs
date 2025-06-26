c2ca058de2da032cf636f99272e011cc596fc730
pranav.j <pranav.j@scripbox.com>
2025-06-27 03:25:31 +0530

    removed history files

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Clipixir.MixProject do
   def project do
     [
       app: :clipixir,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
