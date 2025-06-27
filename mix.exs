3d53571db5302ea814a5b3f18897549d2bfc6134
pranav.j <pranav.j@scripbox.com>
2025-06-27 16:56:57 +0530

    updated version to publish to hexdocs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Clipixir.MixProject do
   def project do
     [
       app: :clipixir,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
